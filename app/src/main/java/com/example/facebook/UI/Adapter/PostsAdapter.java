b6edaceee956d9349271431a7b0c356ed78d1de2
makkawy16 <yousef.elmakkawy@gmail.com>
2022-10-26 10:45:22 +0200

    finish posts adapter

---
+++ app/src/main/java/com/example/facebook/UI/Adapter/PostsAdapter.java
@@ -0,0 +1,58 @@
+package com.example.facebook.UI.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.facebook.Model.PostModel;
+import com.example.facebook.databinding.ItemPostLayoutBinding;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.postViewHolder> {
+    List<PostModel> posts;
+
+    public void addPost(List<PostModel> posts) {
+        this.posts = posts;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public postViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemPostLayoutBinding binding =
+                ItemPostLayoutBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        return new postViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull postViewHolder holder, int position) {
+        PostModel post = posts.get(position);
+        holder.binding.profileName.setText(post.getUserName());
+        holder.binding.postTitle.setText(post.getPostTitle());
+        Picasso.get().load(post.getUserProfilePic()).into(holder.binding.profilePicHome);
+        Picasso.get().load(post.getUploadedPhotoURl()).into(holder.binding.myImg);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return posts == null ? 0 : posts.size();
+    }
+
+
+    static class postViewHolder extends RecyclerView.ViewHolder {
+        ItemPostLayoutBinding binding;
+
+        public postViewHolder(ItemPostLayoutBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/facebook/UI/AddNewPostFragment.java
@@ -0,0 +1,51 @@
+package com.example.facebook.UI;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.facebook.R;
+import com.example.facebook.databinding.FragmentAddNewPostBinding;
+
+public class AddNewPostFragment extends Fragment {
+
+    FragmentAddNewPostBinding binding;
+
+    public AddNewPostFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_new_post, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding = FragmentAddNewPostBinding.bind(view);
+
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
